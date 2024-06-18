8744c21d76552a1867e785afebcb2d121a588675
Wendel Segadilha <wendelsegadilha99@gmail.com>
2024-06-18 14:09:57 -0300

    Criando componentes

---
+++ src/app/components/first-component/first-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrl: './first-component.component.css'
+})
+export class FirstComponentComponent {
+
+}
