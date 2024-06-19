11937b6be59e9174d2bfcf288e1bc02bf0b805a3
Wendel Segadilha <wendelsegadilha99@gmail.com>
2024-06-19 09:47:29 -0300

    A importância das interfaces no Angular

--- src/app/components/list-component/list-component.component.ts
+++ src/app/components/list-component/list-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Animal } from '../../interfaces/animal';
 
 @Component({
   selector: 'app-list-component',
@@ -6,12 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './list-component.component.css',
 })
 export class ListComponentComponent implements OnInit {
-  animals = [
+  animals: Animal[] = [
     { id: 1, name: 'Frida', type: 'Dog', age: 5 },
     { id: 2, name: 'Ping', type: 'Cat', age: 1 },
     { id: 3, name: 'Pong', type: 'Cat', age: 1 },
     { id: 4, name: 'Mokita', type: 'Dog', age: 2 },
   ];
+
+  animal: Animal = {
+    id: 5,
+    name: 'Luna',
+    type: 'Cat',
+    age: 2,
+  };
+
+  mostarNoConsole(animal: Animal) {
+    console.log(animal);
+  }
 
   ngOnInit(): void {}
 }
